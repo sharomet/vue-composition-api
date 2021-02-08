27d8902ae33049329306b472cd9890e53125e99c
asharomet <sharometal@gmail.com>
2021-02-08 17:50:45 +0200

    Updated

---
+++ src/components/users/UserForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row justify-content-center mb-3">
+    <div class="col-xl-5 col-lg-6 col-md-7">
+      <form @submit.prevent="handleSubmit">
+        <div class="form-row">
+          <div class="form-group col-lg-6">
+            <label for="firstName">First Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="firstName"
+              v-model="user.firstName"
+            />
+          </div>
+          <div class="form-group col-lg-6">
+            <label for="lastName">Last Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="lastName"
+              v-model="user.lastName"
+            />
+          </div>
+        </div>
+        <div class="text-right">
+          <button class="btn btn-primary btn-block">Add User</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        firstName: '',
+        lastName: '',
+      }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit('addNewUser', this.user)
+      this.user.firstName = ''
+      this.user.lastName = ''
+
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,19 @@
 <template>
   <div class="container">
-    <div class="row justify-content-center mb-3">
-      <div class="col-xl-5 col-lg-6 col-md-7">
-        <form @submit.prevent="addUser">
-          <div class="form-row">
-            <div class="form-group col-lg-6">
-              <label for="firstName">First Name</label>
-              <input
-                type="text"
-                class="form-control"
-                id="firstName"
-                v-model="firstName"
-              />
-            </div>
-            <div class="form-group col-lg-6">
-              <label for="lastName">Last Name</label>
-              <input
-                type="text"
-                class="form-control"
-                id="lastName"
-                v-model="lastName"
-              />
-            </div>
-          </div>
-          <div class="text-right">
-            <button class="btn btn-primary btn-block">Add User</button>
-          </div>
-        </form>
-      </div>
-    </div>
-
+    <UserForm @addNewUser="addNewUser" />
     <Users :users="usersList" @removeUser="removeUser" />
-
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
-import Users from '@/components/users/users'
+import Users from '@/components/users/Users'
+import UserForm from '@/components/users/UserForm'
 
 export default {
   components: {
-    Users
+    Users,
+    UserForm,
   },
   setup() {
     const usersList = ref([
@@ -51,30 +23,24 @@ export default {
       { id: 4, firstName: 'Viliam', lastName: 'Craken' },
     ])
 
-    const firstName = ref('');
-    const lastName = ref('');
-
     const removeUser = id => {
       usersList.value = usersList.value.filter(users => users.id !== id)
     }
 
-    const addUser = () => {
-      let id = usersList.value.length + 1;
+    const addNewUser = ({ firstName, lastName }) => {
+      let id = usersList.value.length + 1
       usersList.value.push({
-        id, firstName, lastName,
+        id,
+        firstName,
+        lastName,
       })
-      /*firstName.value = ''
-      lastName.value = ''*/
     }
 
     return {
       usersList,
       removeUser,
-      firstName,
-      lastName,
-      addUser
+      addNewUser,
     }
-  }
-
+  },
 }
 </script>

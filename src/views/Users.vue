932d07e977f90862c0f00e59a2071f2885f7ceca
asharomet <sharometal@gmail.com>
2021-02-10 15:05:32 +0200

    Sort user

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -5,15 +5,10 @@
         <label>User Name</label>
         <input type="text" class="form-control" v-model="usersInput" />
       </div>
-      <ul class="search__list">
-        <li v-for="user in users" :key="user.id">
-          {{ user.first_name }}
-        </li>
-      </ul>
     </div>
     <table class="table table-sm table-bordered">
       <tbody>
-        <tr v-for="(user, index) in userData" :key="index">
+        <tr v-for="user in users" :key="user.id">
           <td>
             {{ user.first_name }}
           </td>
@@ -32,53 +27,43 @@
   </div>
 </template>
 
-<style lang="scss" scoped>
-.search {
-  position: relative;
-  &__list {
-    position: absolute;
-    left: 0;
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-    z-index: 1;
-    background-color: #fff;
-    width: 100%;
-    border: 1px solid #ccc;
-    li {
-      padding: 5px 10px;
-    }
-  }
-}
-</style>
-
 <script>
 import { ref, onMounted, watch } from 'vue'
 import axios from 'axios'
 
 export default {
   setup() {
-    let userData = ref([])
+    let users = ref([])
     const getUserData = async () =>
       await axios
         .get('http://localhost:3000/users')
-        .then(res => (userData.value = res.data))
+        .then(res => users.value = sortUser(res.data))
         .catch(err => console.log(err))
-
     onMounted(getUserData)
 
     const usersInput = ref('')
-    const users = ref([])
     watch(usersInput, () => {
-      axios.get('http://localhost:3000/users')
-        .then(res => users.value = res.data)
-        .catch(err => console.log(err))
+      users.value.sort(a => a.first_name.indexOf(usersInput.value) > -1 ? -1 : 0)
+      if(usersInput.value == '') {
+        sortUser(users.value)
+      }
     })
 
+    const sortUser = data => {
+      return data.sort((a, b) => {
+        if(a.first_name < b.first_name) {
+          return -1
+        }
+        if(a.first_name > b.first_name) {
+          return 1
+        }
+        return 0;
+      })
+    }
+
     return {
-      users,
-      userData,
-      usersInput
+      usersInput,
+      users
     }
   },
 }
